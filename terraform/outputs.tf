2ab38ce2768e568d4c85b057e2fb150fb7bfe8b5
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-11-11 20:20:07 +0000

    Updates

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,8 +3,17 @@ locals {
     name                = web_app.name
     resource_group_name = web_app.resource_group_name
   }]
+
+  func_apps = [for func_app in azurerm_linux_function_app.app : {
+    name                = func_app.name
+    resource_group_name = func_app.resource_group_name
+  }]
 }
 
 output "web_apps" {
   value = local.web_apps
+}
+
+output "func_apps" {
+  value = local.func_apps
 }
